9f61339ac05aef3ec51cdbd81790adf4b43a26ed
BorisS <teachandlearn999@gmail.com>
2025-10-27 14:53:30 -0700

    lesson 1

--- Playwright/DemoProject/tests/2025_fall_firstTest.spec.js
+++ Playwright/DemoProject/tests/2025_fall_firstTest.spec.js
@@ -3,12 +3,17 @@ import { pageHeadline } from "./helpers/firstTestData";
 
 test.describe("first test suite", () => {
 	//
-	//
-	//
-	//
-	//
 
 	// ----- lesson 1
+
+	/*
+    test - функция, используется для прогона тестов
+    describe - свойство (property) функции test, группирует все тесты
+    page - объект, отражает конкретную страницу. имеет множество методов
+    expect - функция, используется для проверки. принимает локатор
+    *toEqual - функция, что конкретно проверять. "matcher function", добовляется цепочкой к expect
+    */
+
 	// ----- первый тест
 	test("verify page title", async ({ page }) => {
 		await page.goto("/");
@@ -18,6 +23,25 @@ test.describe("first test suite", () => {
 		console.log(title);
 
 		expect(title).toEqual("Automation Exercise");
+	});
+
+	test("verify homepage", async ({ page }) => {
+		await page.goto("/");
+
+		const homepageLink = page.locator(".navbar-nav li:first-child a");
+		expect(homepageLink).toHaveCSS("color", "rgb(255, 165, 0)");
+	});
+
+	test("verify logo", async ({ page }) => {
+		await page.goto("/");
+
+		expect(page.locator(".logo img")).toHaveAttribute(
+			"src",
+			"/static/images/home/logo.png"
+		);
+
+		// const imgUrl = await page.locator(".logo img").getAttribute("src");
+		// expect(imgUrl).toBe("/static/images/home/logo.png");
 	});
 
 	//
