8ee5c28c75f5bf1e3a9feb2dd0760bcaa57b9dbe
BorisS <teachandlearn999@gmail.com>
2025-11-17 11:53:07 -0800

    lesson 4 changes

--- Playwright/DemoProject/lesson4/tests/test1.spec.js
+++ Playwright/DemoProject/lesson4/tests/test1.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { jenkinsData } from "./testData/jenkinsData";
 
 test.describe("first test suite", () => {
 	// -----------------
@@ -10,28 +11,36 @@ test.describe("first test suite", () => {
 	test("drag and drop", async ({ page }) => {
 		await page.goto("https://www.qa-practice.com/elements/dragndrop/boxes");
 
-		// const sourceEl = page.locator("#rect-draggable");
-		// const destinationEl = page.locator("#rect-droppable");
+		const sourceEl = page.locator("#rect-draggable");
+		const destinationEl = page.locator("#rect-droppable");
 
-		// await sourceEl.dragTo(destinationEl);
+		await sourceEl.dragTo(destinationEl);
 
-		// // проверим, что source теперь внутри destination
-		// await expect(destinationEl).toContainText("Drag me");
+		// проверим, что source теперь внутри destination
+		await expect(destinationEl).toContainText("Drag me");
 
 		//
 		//
 		//
 
-		// --- using mouse actions
-		// Этот метод обеспечивает более детальный контроль и может быть полезен для
-		// более сложных сценариев перетаскивания или когда dragTo() ведет себя не так, как ожидалось.
+		/* 
+            --- using mouse actions
+		    Этот метод обеспечивает более детальный контроль и может быть полезен для
+		    более сложных сценариев перетаскивания или когда dragTo() ведет себя не так,
+		    как ожидалось.
+        */
+
+		// await page.goto("https://www.qa-practice.com/elements/dragndrop/boxes");
 
 		// const sourceEl = page.locator("#rect-draggable");
 		// const destinationEl = page.locator("#rect-droppable");
 
 		// await sourceEl.hover(); // hover over the source element
+
 		// await page.mouse.down(); // press left mouse button
+
 		// await destinationEl.hover(); // hover over the destination element
+
 		// await page.mouse.up(); // release the left mouse button
 
 		// await expect(destinationEl).toContainText("Drag me");
@@ -48,13 +57,13 @@ test.describe("first test suite", () => {
 	test("iframes", async ({ page }) => {
 		await page.goto("https://www.qa-practice.com/elements/iframe/iframe_page");
 
-		// const iframe = page.frameLocator(".embed-responsive-item");
+		const iframe = page.frameLocator(".embed-responsive-item");
 
-		// const buttonInsideIframe = iframe.locator(".btn-primary");
+		const buttonInsideIframe = iframe.locator(".btn-primary");
 
-		// await buttonInsideIframe.click();
+		await buttonInsideIframe.click();
 
-		// await expect(buttonInsideIframe).toBeVisible();
+		await expect(buttonInsideIframe).toBeVisible();
 	});
 
 	//
@@ -97,6 +106,7 @@ test.describe("first test suite", () => {
 	//
 
 	// --- working with auto-opening modal
+	//     (DO NOT SHOW)
 
 	test.skip("auto-opening modal", async ({ page }) => {
 		// Navigate to your page
@@ -115,5 +125,81 @@ test.describe("first test suite", () => {
 		// Playwright will automatically run the handler if the modal appears.
 		await page.locator("your-triggering-element-selector").click();
 		// ... more test steps
+	});
+
+	//
+	//
+
+	// --- working with hover menu on the project items
+
+	test("hover menu on project items", async ({ page }) => {
+		//#region login (already done in jenkins)
+		await page.goto(`http://localhost:8080/`);
+		await page.locator("#j_username").fill("admin");
+		await page.locator('input[name="j_password"]').fill("password");
+		await page.locator('button[name="Submit"]').click();
+
+		await page.waitForLoadState("networkidle");
+		await Promise.race([
+			page
+				.waitForURL((url) => !url.includes("/login"), { timeout: 30000 })
+				.catch(() => null),
+			page
+				.waitForSelector("#jenkins-head-icon", { timeout: 30000 })
+				.catch(() => null),
+		]);
+		//#endregion
+
+		await page.locator("#side-panel a[href$='newJob']").click();
+		await page.locator("#name").fill(jenkinsData.jobName);
+		await page
+			.locator(
+				"#j-add-item-type-standalone-projects ul li[class*='FreeStyleProject']"
+			)
+			.click();
+		await page.locator("#ok-button").click();
+
+		await page.locator(".app-jenkins-logo").click();
+
+		//
+		//
+
+		// 1. Get locator of the job
+		const jobLink = page.getByRole("link", {
+			name: jenkinsData.jobName,
+			exact: true,
+		});
+
+		// 2. Hover the job link (to make the button appear).
+		await jobLink.hover();
+
+		// 3. Get locator of the chevron button
+		const chevronButton = jobLink.getByRole("button");
+
+		// Ensure the button is visible before attempting to focus/click
+		await chevronButton.waitFor({ state: "visible" });
+
+		// 4. Click the button, but also try simulating a Tab/Enter key sequence
+		// to ensure the element receives focus and triggers the dropdown logic.
+		// NOTE: doing a normal hover/click would not work on CI
+		await chevronButton.focus(); // Give the element focus
+		await chevronButton.press("Enter"); // Simulate activation via keyboard
+
+		// 5. Get locator of the popup
+		const popupRoot = page.locator("div[id^='tippy-']");
+
+		// Wait for the popup to display
+		await popupRoot.waitFor({ state: "attached", timeout: 5000 });
+
+		// 6. Get locator of the links inside the popup
+		const popupLinks = popupRoot.locator(
+			`.jenkins-dropdown [href*='${jenkinsData.jobName}']`
+		);
+
+		console.log("popupLinks: ", await popupLinks.allInnerTexts());
+
+		expect(await popupLinks.allInnerTexts()).toContain(
+			jenkinsData.jobPopupOptions.configure
+		);
 	});
 });
