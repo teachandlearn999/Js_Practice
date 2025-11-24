7adf14178a13b9b49377bb74e757d03899ac0147
Boris S <115181391+teachandlearn999@users.noreply.github.com>
2025-11-24 00:38:57 -0800

    Delete Playwright/DemoProject/lesson5-jenkins-POM/tests/test.spec.js

--- Playwright/DemoProject/lesson5-jenkins-POM/tests/test.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "@playwright/test";
-import { test } from "../../base.js";
-
-test.describe("some other test", () => {
-	test("test1", async ({ page }) => {
-		page.locator("b");
-	});
-});
