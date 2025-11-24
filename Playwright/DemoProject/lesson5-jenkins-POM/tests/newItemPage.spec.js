780a92c04251d0d0ecfdf72535db3a9c2f627933
BorisS <teachandlearn999@gmail.com>
2025-11-24 00:07:19 -0800

    added description of the test

--- Playwright/DemoProject/lesson5-jenkins-POM/tests/newItemPage.spec.js
+++ Playwright/DemoProject/lesson5-jenkins-POM/tests/newItemPage.spec.js
@@ -3,6 +3,8 @@ import { test } from "../../base.js";
 import { newItemPageData as pageData } from "../testData/newItemPageData.js";
 
 test.describe("new item page tests", () => {
+	// TC_01.001.09 | New Item > Creatе a new item> "New Item" be accessible (#142)
+
 	test("TC_xxx | Verify new item page load", async ({ homePage }) => {
 		const newItemPage = await homePage.clickNewItemLink();
 		await expect(newItemPage.getLocatorPageName()).toHaveText(
